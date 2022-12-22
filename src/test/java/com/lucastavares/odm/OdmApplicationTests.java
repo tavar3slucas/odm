6ce1a18d03ae185eca74813e1267f9c0b6352db7
t.luuccas@gmail.com <lucas.tavares@petzpartner.com.br>
2022-12-22 19:48:39 -0300

    Criação de Projeto

---
+++ src/test/java/com/lucastavares/odm/OdmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lucastavares.odm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OdmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
