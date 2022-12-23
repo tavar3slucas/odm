d174b299b26d46535f703e943a7bdeab83df3210
t.luuccas@gmail.com <lucas.tavares@petzpartner.com.br>
2022-12-23 14:15:24 -0300

    Criando operação de instanciação

--- src/main/java/com/lucastavares/odm/OdmApplication.java
+++ src/main/java/com/lucastavares/odm/OdmApplication.java
@@ -1,13 +1,30 @@
 package com.lucastavares.odm;
 
+import com.lucastavares.odm.domain.Categoria;
+import com.lucastavares.odm.repository.CategoriaRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class OdmApplication {
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
+@SpringBootApplication
+public class OdmApplication implements CommandLineRunner {
+
+	@Autowired
+	private CategoriaRepository categoriaRepository;
 	public static void main(String[] args) {
 		SpringApplication.run(OdmApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+
+		Categoria cat1 = new Categoria(null,"Informática");
+		Categoria cat2 = new Categoria(null,"Escritório");
+		categoriaRepository.saveAll(Arrays.asList(cat1,cat2));
+	}
 }
--- src/main/java/com/lucastavares/odm/controller/CategoriaController.java
+++ src/main/java/com/lucastavares/odm/controller/CategoriaController.java
@@ -21,9 +21,7 @@ public class CategoriaController {
 
     @RequestMapping(value = "/{id}", method = RequestMethod.GET)
     public ResponseEntity<?> find(@PathVariable Integer id){
-
         Categoria obj = service.buscar(id);
         return ResponseEntity.ok().body(obj);
-
     }
 }
